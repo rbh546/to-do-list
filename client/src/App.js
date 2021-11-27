80e82edbf71d032b373848451d11ad922adff1d1
ridwa <rbh546@mun.ca>
2021-11-27 19:59:04 -0330

    up to delete

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,19 @@
-import React, {Fragment} from "react";
-import './App.css';
+import React, { Fragment } from "react";
+import "./App.css";
+
+// components
+
+import InputTodo from "./components/InputTodo.js";
+import ListTodos from "./components/ListTodo.js";
 
 function App() {
   return (
-    <Fragment></Fragment>
+    <Fragment>
+      <div className="container">
+        <InputTodo />
+        <ListTodos />
+      </div>
+    </Fragment>
   );
 }
 
